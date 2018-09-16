59508ee27e0e12ad49a124eb1daa1a1b3c5160e6
Your Name <scottstewart@Scotts-MacBook-Air.local>
2018-09-16 16:06:50 -0400

    risk manager inputs

--- pages/components/calculator/index.js
+++ pages/components/calculator/index.js
@@ -1,6 +1,8 @@
 import BaseComponent from '../../lib/BaseComponent';
 
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 import withStyles from './styles';
 
@@ -10,7 +12,60 @@ class Calculator extends BaseComponent {
 
 		return (
 			<div className={classes.root}>
-				<Typography align="center" variant="title">Risk Manager</Typography>
+				<Typography align="center" variant="title">
+					Risk Manager
+				</Typography>
+				<TextField
+					align="left"
+					label="Position Size"
+					fullWidth={true}
+					className={classes.textField}
+					value={this.state.positionSize}
+					InputProps={{
+						startAdornment: <InputAdornment position="start">$</InputAdornment>
+					}}
+				/>
+				<TextField
+					align="left"
+					label="Risk Amount"
+					InputProps={{
+						startAdornment: <InputAdornment position="start">%</InputAdornment>
+					}}
+					className={classes.textField}
+					value={this.state.riskAmount}
+					fullWidth={true}
+				/>
+				<TextField
+					align="left"
+					label="Entry Price"
+					InputProps={{
+						startAdornment: <InputAdornment position="start">$</InputAdornment>
+					}}
+					className={classes.textField}
+					value={this.state.entryPrice}
+					fullWidth={true}
+				/>
+				<TextField
+					align="left"
+					label="Target Price"
+					InputProps={{
+						startAdornment: <InputAdornment position="start">$</InputAdornment>
+					}}
+					className={classes.textField}
+					value={this.state.targetPrice}
+					fullWidth={true}
+				>
+					<TextField
+						align="left"
+						label="Stop price"
+						InputProps={{
+							startAdornment: <InputAdornment position="start">$</InputAdornment>
+						}}
+						className={classes.textField}
+						value={this.state.stopPrice}
+						fullWidth={true}
+					/>
+				</TextField>
 			</div>
 		);
 	}
